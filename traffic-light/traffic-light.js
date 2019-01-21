c921c7cb0b7cd9649946c0474bf2d3b55717c330
alinavalovenko <alina.valovenko@gmail.com>
2019-01-21 17:58:50 +0200

    first commit

--- traffic-light/traffic-light.js
+++ traffic-light/traffic-light.js
@@ -1,7 +1,28 @@
-$(document).ready(function () {
-    var title = $('h1.title');
-    var description = $('p.description');
-    var results = $('div.results');
+var trafficLightElemens = document.querySelectorAll('.traffic-light .traffic-light-lamp');
+var redElem = trafficLightElemens[0];
+var yellowElem = trafficLightElemens[1];
+var greenElem = trafficLightElemens[2];
 
+var btnStart = document.querySelector('.traffic-light-start');
+var btnPause = document.querySelectorAll('.traffic-light-pause');
+var btnStop = document.querySelectorAll('.traffic-light-stop');
 
-});
+btnStart.onclick = function () {
+
+    var timerRed  = setTimeout(function () {
+        redElem.classList.add('red');
+    }, 5000);
+    var timerYellow = setTimeout(function () {
+        clearInterval(timerRed);
+        redElem.classList.remove('red');
+        yellowElem.classList.add('yellow');
+    }, 3000);
+
+    var timerGreen = setTimeout(function () {
+        clearInterval(timerYellow);
+        yellowElem.classList.remove('yellow');
+
+        greenElem.classList.add('green');
+    }, 5000);
+
+};
